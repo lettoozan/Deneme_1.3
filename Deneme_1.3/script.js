2848bfcddc1cf503a86b26142f68169e746b5c69
lettoozan <lettoozan08@gmail.com>
2021-12-08 00:25:10 +0300

    add clickable dropdown function

--- Deneme_1.3/script.js
+++ Deneme_1.3/script.js
@@ -3,3 +3,14 @@ function searchWordOnGoogle(){
     var url = "https://www.google.com/search?q=" + searchWord;
     window.open(url,"_self");
 }
+function settings_click(){
+    document.getElementById("settings_dropdown").classList.toggle("show");
+}
+window.onclick = function(event){
+    if (!event.target.matches('.settings_style')){
+        var dropdowns = document.getElementsByClassName("settings_tools");
+        for(let i = 0; i < dropdowns.length; i++){
+            dropdowns[0].classList.remove();
+        }
+    }
+}
