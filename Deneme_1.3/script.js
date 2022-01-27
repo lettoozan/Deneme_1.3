474213d51c90e8dae029133fb241959a4211b872
lettoozan <lettoozan08@gmail.com>
2022-01-27 14:09:15 +0300

    Fix some bugs and performance improvement

--- Deneme_1.3/script.js
+++ Deneme_1.3/script.js
@@ -3,10 +3,10 @@ function searchWordOnGoogle(){
     var url = "https://www.google.com/search?q=" + searchWord;
     window.open(url,"_self");
 }
-function settings_click(){
-    document.getElementById("settings_dropdown").classList.toggle("show");
+function settings_click(){//made clickable dropdown manu
+    document.getElementById("settings_dropdown").classList.toggle("show");//for open the settings dropdown menu
 }
-window.onclick = function(event){
+window.onclick = function(event){//for close the settings dropdown menu
     if (!event.target.matches('.settings_style')){
         var dropdowns = document.getElementsByClassName("settings_tools");
         for(let i = 0; i < dropdowns.length; i++){
